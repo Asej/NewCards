b4749ff275824eab58727f4f03adb54b11daa701
anthonyedwards <anthonydwrds8@gmail.com>
2018-11-10 15:34:38 -0500

    Initial Commit

---
+++ NewFlash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewFlash
+//
+//  Created by Anthony Edwards on 11/10/18.
+//  Copyright © 2018 Anthony Edwards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
